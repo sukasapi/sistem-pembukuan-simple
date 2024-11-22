82c1664bab86e80a1f21b638e046926524e22ed2
sukasapi <30621677+sukasapi@users.noreply.github.com>
2024-11-22 14:51:06 +0700

    Update keuangan_db.sql

--- database/keuangan_db.sql
+++ database/keuangan_db.sql
@@ -1,29 +1,50 @@
+/*
+SQLyog Community v13.3.0 (64 bit)
+MySQL - 8.0.30 : Database - sia_ukdw
+*********************************************************************
+*/
 
+/*!40101 SET NAMES utf8 */;
 
-CREATE TABLE `category` (
-  `category_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`sia_ukdw` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `sia_ukdw`;
+
+/*Table structure for table `ukdw_akun` */
+
+DROP TABLE IF EXISTS `ukdw_akun`;
+
+CREATE TABLE `ukdw_akun` (
+  `category_id` int unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `description` varchar(255) DEFAULT NULL,
   `type` enum('in','out') NOT NULL,
   `status` enum('active','inactive') NOT NULL DEFAULT 'active',
-  PRIMARY KEY (`category_id`)
+  PRIMARY KEY (`category_id`),
+  KEY `nama_akun` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
-/*Data for the table `category` */
+/*Data for the table `ukdw_akun` */
 
-insert  into `category`(`category_id`,`name`,`description`,`type`,`status`) values 
+insert  into `ukdw_akun`(`category_id`,`name`,`description`,`type`,`status`) values 
 (4,'test',NULL,'in','inactive'),
 (5,'0ktest','test','out','inactive'),
 (6,'Test','test','out','active'),
 (7,'0ktest','test','in','active');
 
-/*Table structure for table `transaction` */
+/*Table structure for table `ukdw_transaksi` */
 
-DROP TABLE IF EXISTS `transaction`;
+DROP TABLE IF EXISTS `ukdw_transaksi`;
 
-CREATE TABLE `transaction` (
-  `transaction_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `category_id` int(11) unsigned NOT NULL,
+CREATE TABLE `ukdw_transaksi` (
+  `transaction_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `category_id` int unsigned NOT NULL,
   `create_date` datetime NOT NULL,
   `nominal` float NOT NULL,
   `description` varchar(255) DEFAULT NULL,
@@ -31,9 +52,9 @@ CREATE TABLE `transaction` (
   PRIMARY KEY (`transaction_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
-/*Data for the table `transaction` */
+/*Data for the table `ukdw_transaksi` */
 
-insert  into `transaction`(`transaction_id`,`category_id`,`create_date`,`nominal`,`description`,`status`) values 
+insert  into `ukdw_transaksi`(`transaction_id`,`category_id`,`create_date`,`nominal`,`description`,`status`) values 
 (1,6,'2019-07-30 11:03:22',0,'','inactive'),
 (2,7,'2019-07-30 11:03:22',100000,'test','active'),
 (3,6,'2019-07-30 11:09:38',1000,'keluar','active');
